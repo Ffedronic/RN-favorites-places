702eed6fecdab677457d76a61976e455cf9f096e
Your Name <you@example.com>
2023-12-19 11:59:53 +0100

    getting start custom form

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, TextInput } from "react-native";
 import { Text, View } from "react-native";
+import { Colors } from "../../constants/color";
 
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
 export default PlaceForm;
